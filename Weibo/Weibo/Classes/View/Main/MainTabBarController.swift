2aba169d9376c40e38fc41e623c61574fbdb3956
Xiaoping Weng <xiao5612327@gmail.com>
2020-06-15 11:53:08 -0700

    change tabbar image and title color and font

--- Weibo/Weibo/Classes/View/Main/MainTabBarController.swift
+++ Weibo/Weibo/Classes/View/Main/MainTabBarController.swift
@@ -62,6 +62,9 @@ extension MainTabBarController {
         vc.tabBarItem.image = UIImage(named: "tabbar_" + imageName)
         vc.tabBarItem.selectedImage = UIImage(named: "tabbar_" + imageName + "_selected")?.withRenderingMode(.alwaysOriginal)
         
+        //4. setting tab bar title color
+        vc.tabBarItem.setTitleTextAttributes([NSAttributedString.Key.foregroundColor : UIColor.orange], for: .highlighted)
+        vc.tabBarItem.setTitleTextAttributes([NSAttributedString.Key.font : UIFont.systemFont(ofSize: 14)], for: .normal)
         let nav = MainNavigationController(rootViewController: vc)
         return nav
     }
