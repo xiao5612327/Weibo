0317d6697231e373b3c6309faa236dbe9e98cb2d
Xiaoping Weng <xiao5612327@gmail.com>
2020-06-18 15:55:05 -0700

    Auto fill user account and password.

--- Weibo/Weibo/Classes/View/Main/OAuth/OAuthViewController.swift
+++ Weibo/Weibo/Classes/View/Main/OAuth/OAuthViewController.swift
@@ -21,7 +21,8 @@ class OAuthViewController: UIViewController {
         setupNavigationBar()
         
         let urlString = "https://api.weibo.com/oauth2/authorize?client_id=\(AppKey)&redirect_uri=\(RedirectURL)"
-        
+        //https://api.weibo.com/oauth2/authorize?client_id=2551131061&redirect_uri=http://baidu.com
+
         guard let url = URL(string: urlString) else {
             return
         }
@@ -34,7 +35,18 @@ class OAuthViewController: UIViewController {
     private func setupNavigationBar() {
         title = "Log in Weibo"
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Back", target: self, action: #selector(handleBack), isBackButton: true)
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Auto Fill", target: self, action: #selector(autoFill))
     }
+    
+    @objc private func autoFill() {
+        let js = "document.getElementById('userId').value = 'xiao5612327';" + "document.getElementById('passwd').value = 'aaaa'"
+        
+        webView.evaluateJavaScript(js) { (json, error) in
+            print(error)
+            print(json)
+        }
+    } 
     
     @objc private func handleBack() {
         dismiss(animated: true, completion: nil)
